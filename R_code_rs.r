d55ec12c29d5ea21bdb17a0e1f23977dcb76eff8
igordarin <62661918+igordarin@users.noreply.github.com>
2020-04-24 18:05:18 +0200

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -43,3 +43,57 @@ plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
 # exercise NIR on top of the G component
 plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
 plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
+
+p224r63_1988 <- brick("p224r63_1988_masked.grd")
+
+par(mfrow=c(2,1))
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+
+par(mfrow=c(2,1))
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="hist")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="hist")
+
+# exercise: plot in visible colour 321 both images     
+par(mfrow=c(2,1))
+plotRGB(p224r63_1988, r=3, g=2, b=1, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+
+# exercise: plot in  colour 432 both images  
+par(mfrow=c(2,1))
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+     
+# enhance the noise
+par(mfrow=c(2,1))
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="hist") 
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="hist")
+ 
+# dvi difference vegetation index
+# RGB bands
+# B1: blue
+# B2: green
+# B3: red <-B3_sre
+# B4: NIR <- B4_sre
+dvi2011 <- p224r63_2011$B4_sre - p224r63_2011$B3_sre
+cl <- colorRampPalette(c('"darkorchid3"','light blue','lightpink4'))(100) 
+plot(dvi2011)
+
+dvi1988 <- p224r63_1988$B4_sre - p224r63_1988$B3_sre
+plot(dvi1988, col= cl)
+
+#difference from one year to the other
+diff <- dvi2011 - dvi1988
+plot(diff)
+     
+# changing the grain (resolution)
+p224r63_2011res <- aggregate(p224r63_2011, fact=10)
+p224r63_2011res100 <- aggregate(p224r63_2011, fact=100)
+
+par(mfrow=c(3,1))
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011res, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011res100, r=4, g=3, b=2, stretch="Lin")
+p224r63_2011 # info
+p224r63_2011res100
+     
